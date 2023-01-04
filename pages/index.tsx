6a4cdcb8f53f82b867d8f3dcb0417878d1c4f8ed
Luna Goodnight <luna.lee@nextfortune.tw>
2023-01-04 21:58:36 +0800

    [Add] init

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.head}></div>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
