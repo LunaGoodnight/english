8284e36247d374299acbe1f73e80df71bf4d64b7
Luna Goodnight <luna.lee@nextfortune.tw>
2023-02-21 00:01:34 +0800

    [Add] head info

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>English</title>
+        <title>English Learning Resources - Index</title>
         <meta name="description" content="Path to learn English" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
--- pages/sites.tsx
+++ pages/sites.tsx
@@ -2,36 +2,45 @@ import { SiteNavigation } from "../common/SiteNavigation";
 import { siteList } from "../configs/siteList";
 import { Header } from "../common/Header";
 import Image from "next/image";
+import Head from "next/head";
 
 export const sites = () => {
   return (
-    <div>
-      <Header />
-      <div className="flex">
-        <SiteNavigation />
-        <ul className="w-1/5 w-full">
-          {siteList.map((x, i) => {
-            const { title, link, image } = x;
-            return (
-              <li key={i} className="p-8 flex">
-                <div className="flex w-48">
-                  <Image
-                    src={image}
-                    alt={title}
-                    width={350}
-                    height={219}
-                    priority
-                  />
-                </div>
-                <div className="pl-5">
-                  <a href={link}>{title}</a>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+    <>
+      <Head>
+        <title>Sites</title>
+        <meta name="description" content="一些學英文的網站" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Header />
+        <div className="flex">
+          <SiteNavigation />
+          <ul className="w-1/5 w-full">
+            {siteList.map((x, i) => {
+              const { title, link, image } = x;
+              return (
+                <li key={i} className="p-8 flex">
+                  <div className="flex w-48">
+                    <Image
+                      src={image}
+                      alt={title}
+                      width={350}
+                      height={219}
+                      priority
+                    />
+                  </div>
+                  <div className="pl-5">
+                    <a href={link}>{title}</a>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
