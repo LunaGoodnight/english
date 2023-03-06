52299b9f7af4283b1049cfaaef8894e48b1983b8
Luna Goodnight <luna.lee@nextfortune.tw>
2023-03-06 16:15:32 +0800

    [Add] new links

--- configs/youtubeList.tsx
+++ configs/youtubeList.tsx
@@ -1,10 +1,16 @@
 import Kurzgesagt from "../public/images/kurzgesagt.webp";
-
+import BetterThanYesterday from "../public/images/betterThanYesterday.webp";
 export const youtubeList = [
   {
     title: "Kurzgesagt",
     link: "https://www.youtube.com/@kurzgesagt",
     image: Kurzgesagt,
+    tags: [],
+  },
+  {
+    title: "Better Than Yesterday",
+    link: "https://www.youtube.com/@BetterThanYesterday",
+    image: BetterThanYesterday,
     tags: [],
   },
 ];
